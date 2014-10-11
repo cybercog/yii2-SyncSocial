401ef3c4a016eae319caa6d1d89c884624b7962b
Sergey Ivanov <xifrin@gmail.com>
2014-10-11 18:30:02 +0800

    initial tests commit

---
+++ tests/codeception/unit/components/SynchonizerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace tests\codeception\unit\components;
+
+use Yii;
+use yii\base\ErrorException;
+use yii\codeception\TestCase;
+use xifrin\SyncSocial\components\Synchronizer;
+
+/**
+ * @inheritdoc
+ */
+class SynchonizerTest extends \yii\codeception\TestCase
+{
+    public $appConfig = '@tests/codeception/config/unit.php';
+
+
+    public function setUp(){
+
+    }
+    public function testInitialComponent()
+    {
+        $services = [
+            'social_1' => [],
+            'social_2' => [],
+            'social_3' => [],
+            'social_4' => [],
+        ];
+
+        $this->setExpectedException('ErrorException');
+
+        $synchronizer = new Synchronizer( array(
+            'services' => $services
+        ) );
+
+
+        $list = $synchronizer->getServiceList();
+        $this->assertTrue(array_keys($services) === array_keys($list));
+    }
+}

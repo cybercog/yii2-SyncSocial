83738874b651c1e08d0d22d56aececef0130c6a3
Sergey Ivanov <xifrin@gmail.com>
2014-10-24 14:28:59 +0800

    update Sync action

--- src/actions/RunAction.php
+++ src/actions/RunAction.php
@@ -16,13 +16,13 @@ class RunAction extends ActionSynchronize {
     public function run($service)
     {
         if ($this->synchronizer->isConnected($service)){
-
-            // @TODO: collect last posts
-            $service = $this->synchronizer->getService($service);
-            $service->getPosts();
-
-            Yii::$app->session->setFlash('success', Yii::t('SyncSocial', 'Service was successfully synchronized'));
-            $this->controller->redirect( $this->successUrl );
+            if ($this->synchronizer->syncService($service)){
+                Yii::$app->session->setFlash('success', Yii::t('SyncSocial', 'Service was successfully synchronized'));
+                $this->controller->redirect( $this->successUrl );
+            } else {
+                Yii::$app->session->setFlash('warning', Yii::t('SyncSocial', 'There is a error in service synchronization'));
+                $this->controller->redirect( $this->failedUrl );
+            }
         } else {
             Yii::$app->session->setFlash('warning', Yii::t('SyncSocial', 'Service is not connected'));
             $this->controller->redirect( $this->failedUrl );

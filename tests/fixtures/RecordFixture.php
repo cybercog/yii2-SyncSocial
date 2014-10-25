ad552793a3d7c89196c5560b4d1956282e532926
Sergey Ivanov <xifrin@gmail.com>
2014-10-26 01:56:50 +0800

    add empty fixtures

---
+++ tests/fixtures/RecordFixture.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace tests\fixtures;
+
+use yii\test\ActiveFixture;
+use xifrin\SyncSocial\behavoirs\SynchronizerBehavior;
+
+class RecordFixture extends ActiveFixture
+{
+    public $modelClass = '\tests\models\Record';
+
+}

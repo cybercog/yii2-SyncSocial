a859986914ff64077a6ff0fb6b9eaf0241502b7f
Sergey Ivanov <xifrin@gmail.com>
2014-10-26 01:57:16 +0800

    update _bootstrap

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -3,6 +3,7 @@
 date_default_timezone_set( 'Asia/Krasnoyarsk' );
 
 defined( 'YII_DEBUG' ) or define( 'YII_DEBUG', true );
+defined( 'YII_ENABLE_ERROR_HANDLER' ) or define( 'YII_ENABLE_ERROR_HANDLER', false );
 
 require_once( __DIR__ . '/../vendor/autoload.php' );
 require_once( __DIR__ . '/../vendor/yiisoft/yii2/Yii.php' );

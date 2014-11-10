7b0366f9dece673b29fdf9bc5d5716235bc547c9
Sergey Ivanov <xifrin@gmail.com>
2014-11-10 10:18:48 +0700

    fix phpdoc

--- src/SyncService.php
+++ src/SyncService.php
@@ -25,7 +25,7 @@ class SyncService extends Object implements ISyncService {
     protected $options = [];
 
     /**
-     * @param array $service
+     * @param \OAuth\Common\Service\ServiceInterface $service
      * @param array $options
      */
     public function __construct( $service, $options = [] ) {
